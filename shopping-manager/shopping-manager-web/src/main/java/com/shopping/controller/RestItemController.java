2ac655bc2f779783317d8300b05431acb3ce7449
许立煌 <xu.lh@neusoft.com>
2020-03-20 15:45:20 +0800

    完善商城修改功能

--- shopping-manager/shopping-manager-web/src/main/java/com/shopping/controller/RestItemController.java
+++ shopping-manager/shopping-manager-web/src/main/java/com/shopping/controller/RestItemController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
